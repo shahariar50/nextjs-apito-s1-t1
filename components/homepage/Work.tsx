3125548482f65dde5b2b0701d511b56f67c5a462
shahariar50 <shahariar.touhid@gmail.com>
2023-03-11 10:52:29 +0600

    functional the gallery

--- components/homepage/Work.tsx
+++ components/homepage/Work.tsx
@@ -2,254 +2,150 @@ import { useState } from "react";
 import { Element } from "react-scroll";
 
 const Work = () => {
-  const [selectedGallery, setSelectedGallery] = useState();
+  const [selectedGallery, setSelectedGallery] = useState("all");
+
+  const galleryList = [
+    {
+      id: 1,
+      img: "/static/img-1.jpg",
+      title: "Work 01",
+      category: "Website",
+      category_slag: ["all", "software"],
+    },
+    {
+      id: 2,
+      img: "/static/img-2.jpg",
+      title: "Work 02",
+      category: "Animation",
+      category_slag: ["all", "graphicDesign"],
+    },
+    {
+      id: 3,
+      img: "/static/img-3.jpg",
+      title: "Work 03",
+      category: "Illustration",
+      category_slag: ["all", "graphicDesign"],
+    },
+    {
+      id: 4,
+      img: "/static/img-4.jpg",
+      title: "Work 04",
+      category: "Application",
+      category_slag: ["all", "apps"],
+    },
+    {
+      id: 5,
+      img: "/static/img-5.jpg",
+      title: "Work 05",
+      category: "Graphic, Logo",
+      category_slag: ["all", "graphicDesign"],
+    },
+    {
+      id: 6,
+      img: "/static/img-6.jpg",
+      title: "Work 06",
+      category: "Web Design",
+      category_slag: ["all", "webDesign"],
+    },
+  ];
 
   return (
     <Element name="work" className="colorlib-work" data-section="work">
       <div className="colorlib-narrow-content">
         <div className="row">
           <div
-            className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+            className="col-md-6 col-md-offset-3 col-md-pull-3"
             data-animate-effect="fadeInLeft"
           >
             <span className="heading-meta">My Work</span>
-            <h2 className="colorlib-heading animate-box">Recent Work</h2>
+            <h2 className="colorlib-heading">Recent Work</h2>
           </div>
         </div>
         <div
-          className="row row-bottom-padded-sm animate-box"
+          className="row row-bottom-padded-sm"
           data-animate-effect="fadeInLeft"
         >
           <div className="col-md-12">
             <p className="work-menu">
-              <span>
-                <a href="#" className="active">
-                  Graphic Design
-                </a>
+              <span
+                className={selectedGallery === "all" ? "active" : ""}
+                onClick={() => setSelectedGallery("all")}
+              >
+                All
               </span>{" "}
-              <span>
-                <a href="#">Web Design</a>
+              <span
+                className={selectedGallery === "graphicDesign" ? "active" : ""}
+                onClick={() => setSelectedGallery("graphicDesign")}
+              >
+                Graphic Design
+              </span>
+              <span
+                className={selectedGallery === "webDesign" ? "active" : ""}
+                onClick={() => setSelectedGallery("webDesign")}
+              >
+                Web Design
               </span>{" "}
-              <span>
-                <a href="#">Software</a>
+              <span
+                className={selectedGallery === "software" ? "active" : ""}
+                onClick={() => setSelectedGallery("software")}
+              >
+                Software
               </span>{" "}
-              <span>
-                <a href="#">Apps</a>
+              <span
+                className={selectedGallery === "apps" ? "active" : ""}
+                onClick={() => setSelectedGallery("apps")}
+              >
+                Apps
               </span>
             </p>
           </div>
         </div>
         <div className="row">
-          <div
-            className="col-md-6 animate-box"
-            data-animate-effect="fadeInLeft"
-          >
-            <div
-              className="project"
-              style={{ backgroundImage: "url(/static/img-1.jpg)" }}
-            >
-              <div className="desc">
-                <div className="con">
-                  <h3>
-                    <a href="work.html">Work 01</a>
-                  </h3>
-                  <span>Website</span>
-                  <p className="icon">
-                    <span>
-                      <a href="#">
-                        <i className="icon-share3"></i>
-                      </a>
-                    </span>
-                    <span>
-                      <a href="#">
-                        <i className="icon-eye"></i> 100
-                      </a>
-                    </span>
-                    <span>
-                      <a href="#">
-                        <i className="icon-heart"></i> 49
-                      </a>
-                    </span>
-                  </p>
+          {galleryList
+            .filter((gallery) =>
+              gallery.category_slag.includes(selectedGallery)
+            )
+            .map((gallery) => (
+              <div
+                className="col-md-6"
+                data-animate-effect="fadeInLeft"
+                key={gallery.id}
+              >
+                <div
+                  className="project"
+                  style={{ backgroundImage: `url(${gallery.img})` }}
+                >
+                  <div className="desc">
+                    <div className="con">
+                      <h3>
+                        <a href="work.html">{gallery.title}</a>
+                      </h3>
+                      <span>{gallery.category}</span>
+                      <p className="icon">
+                        <span>
+                          <a href="#">
+                            <i className="icon-share3"></i>
+                          </a>
+                        </span>
+                        <span>
+                          <a href="#">
+                            <i className="icon-eye"></i> 100
+                          </a>
+                        </span>
+                        <span>
+                          <a href="#">
+                            <i className="icon-heart"></i> 49
+                          </a>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-          <div
-            className="col-md-6 animate-box"
-            data-animate-effect="fadeInRight"
-          >
-            <div
-              className="project"
-              style={{ backgroundImage: "url(/static/img-2.jpg)" }}
-            >
-              <div className="desc">
-                <div className="con">
-                  <h3>
-                    <a href="work.html">Work 02</a>
-                  </h3>
-                  <span>Animation</span>
-                  <p className="icon">
-                    <span>
-                      <a href="#">
-                        <i className="icon-share3"></i>
-                      </a>
-                    </span>
-                    <span>
-                      <a href="#">
-                        <i className="icon-eye"></i> 100
-                      </a>
-                    </span>
-                    <span>
-                      <a href="#">
-                        <i className="icon-heart"></i> 49
-                      </a>
-                    </span>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6 animate-box" data-animate-effect="fadeInTop">
-            <div
-              className="project"
-              style={{ backgroundImage: "url(/static/img-3.jpg)" }}
-            >
-              <div className="desc">
-                <div className="con">
-                  <h3>
-                    <a href="work.html">Work 03</a>
-                  </h3>
-                  <span>Illustration</span>
-                  <p className="icon">
-                    <span>
-                      <a href="#">
-                        <i className="icon-share3"></i>
-                      </a>
-                    </span>
-                    <span>
-                      <a href="#">
-                        <i className="icon-eye"></i> 100
-                      </a>
-                    </span>
-                    <span>
-                      <a href="#">
-                        <i className="icon-heart"></i> 49
-                      </a>
-                    </span>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div
-            className="col-md-6 animate-box"
-            data-animate-effect="fadeInBottom"
-          >
-            <div
-              className="project"
-              style={{ backgroundImage: "url(/static/img-4.jpg)" }}
-            >
-              <div className="desc">
-                <div className="con">
-                  <h3>
-                    <a href="work.html">Work 04</a>
-                  </h3>
-                  <span>Application</span>
-                  <p className="icon">
-                    <span>
-                      <a href="#">
-                        <i className="icon-share3"></i>
-                      </a>
-                    </span>
-                    <span>
-                      <a href="#">
-                        <i className="icon-eye"></i> 100
-                      </a>
-                    </span>
-                    <span>
-                      <a href="#">
-                        <i className="icon-heart"></i> 49
-                      </a>
-                    </span>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div
-            className="col-md-6 animate-box"
-            data-animate-effect="fadeInLeft"
-          >
-            <div
-              className="project"
-              style={{ backgroundImage: "url(/static/img-5.jpg)" }}
-            >
-              <div className="desc">
-                <div className="con">
-                  <h3>
-                    <a href="work.html">Work 05</a>
-                  </h3>
-                  <span>Graphic, Logo</span>
-                  <p className="icon">
-                    <span>
-                      <a href="#">
-                        <i className="icon-share3"></i>
-                      </a>
-                    </span>
-                    <span>
-                      <a href="#">
-                        <i className="icon-eye"></i> 100
-                      </a>
-                    </span>
-                    <span>
-                      <a href="#">
-                        <i className="icon-heart"></i> 49
-                      </a>
-                    </span>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div
-            className="col-md-6 animate-box"
-            data-animate-effect="fadeInRight"
-          >
-            <div
-              className="project"
-              style={{ backgroundImage: "url(/static/img-6.jpg)" }}
-            >
-              <div className="desc">
-                <div className="con">
-                  <h3>
-                    <a href="work.html">Work 06</a>
-                  </h3>
-                  <span>Web Design</span>
-                  <p className="icon">
-                    <span>
-                      <a href="#">
-                        <i className="icon-share3"></i>
-                      </a>
-                    </span>
-                    <span>
-                      <a href="#">
-                        <i className="icon-eye"></i> 100
-                      </a>
-                    </span>
-                    <span>
-                      <a href="#">
-                        <i className="icon-heart"></i> 49
-                      </a>
-                    </span>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+            ))}
         </div>
         <div className="row">
-          <div className="col-md-12 animate-box">
+          <div className="col-md-12">
             <p>
               <a href="#" className="btn btn-primary btn-lg btn-load-more">
                 Load more <i className="icon-reload"></i>
