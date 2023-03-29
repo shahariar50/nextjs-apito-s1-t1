493522d67eba85703aca64022faf37e493553d36
shahariar50 <shahariar.touhid@gmail.com>
2023-03-30 04:32:57 +0600

    setup article api

--- components/homepage/Experence.tsx
+++ components/homepage/Experence.tsx
@@ -1,151 +1,69 @@
+import { useQuery } from "@apollo/client";
 import { Element } from "react-scroll";
+import { JACKSON_EXPERIENCE_DATA } from "../../graphql/homepage.query";
 
 const Experence = () => {
+  const { data } = useQuery(JACKSON_EXPERIENCE_DATA);
+
   return (
     <Element
       name="experience"
       className="colorlib-experience"
       data-section="experience"
     >
-      <div className="colorlib-narrow-content">
-        <div className="row">
-          <div
-            className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
-            data-animate-effect="fadeInLeft"
-          >
-            <span className="heading-meta">Experience</span>
-            <h2 className="colorlib-heading animate-box">Work Experience</h2>
+      {data && (
+        <div className="colorlib-narrow-content">
+          <div className="row">
+            <div
+              className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+              data-animate-effect="fadeInLeft"
+            >
+              <span className="heading-meta">Experience</span>
+              <h2 className="colorlib-heading animate-box">Work Experience</h2>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12">
-            <div className="timeline-centered">
-              <article
-                className="timeline-entry animate-box"
-                data-animate-effect="fadeInLeft"
-              >
-                <div className="timeline-entry-inner">
-                  <div className="timeline-icon color-1">
-                    <i className="icon-pen2"></i>
-                  </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="timeline-centered">
+                {data.experiences.map((exprience: any, index: number) => (
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                    key={exprience.id}
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className={`timeline-icon color-${index}`}>
+                        <i className="icon-pen2"></i>
+                      </div>
 
-                  <div className="timeline-label">
-                    <h2>
-                      <a href="#">Full Stack Developer</a>{" "}
-                      <span>2017-2018</span>
-                    </h2>
-                    <p>
-                      Tolerably earnestly middleton extremely distrusts she boy
-                      now not. Add and offered prepare how cordial two promise.
-                      Greatly who affixed suppose but enquire compact prepare
-                      all put. Added forth chief trees but rooms think may.
-                    </p>
-                  </div>
-                </div>
-              </article>
+                      <div className="timeline-label">
+                        <h2>
+                          <a href="#">{exprience.data.title}</a>{" "}
+                          <span>{exprience.data.from_and_to_date}</span>
+                        </h2>
+                        <p
+                          dangerouslySetInnerHTML={{
+                            __html: exprience.data.details.html,
+                          }}
+                        ></p>
+                      </div>
+                    </div>
+                  </article>
+                ))}
 
-              <article
-                className="timeline-entry animate-box"
-                data-animate-effect="fadeInRight"
-              >
-                <div className="timeline-entry-inner">
-                  <div className="timeline-icon color-2">
-                    <i className="icon-pen2"></i>
+                <article
+                  className="timeline-entry begin animate-box"
+                  data-animate-effect="fadeInBottom"
+                >
+                  <div className="timeline-entry-inner">
+                    <div className="timeline-icon color-none"></div>
                   </div>
-                  <div className="timeline-label">
-                    <h2>
-                      <a href="#">Front End Developer at Google Company</a>{" "}
-                      <span>2017-2018</span>
-                    </h2>
-                    <p>
-                      Even the all-powerful Pointing has no control about the
-                      blind texts it is an almost unorthographic life One day
-                      however a small line of blind text by the name of Lorem
-                      Ipsum decided to leave for the far World of Grammar.
-                    </p>
-                  </div>
-                </div>
-              </article>
-
-              <article
-                className="timeline-entry animate-box"
-                data-animate-effect="fadeInLeft"
-              >
-                <div className="timeline-entry-inner">
-                  <div className="timeline-icon color-3">
-                    <i className="icon-pen2"></i>
-                  </div>
-                  <div className="timeline-label">
-                    <h2>
-                      <a href="#">System Analyst</a> <span>2017-2018</span>
-                    </h2>
-                    <p>
-                      Even the all-powerful Pointing has no control about the
-                      blind texts it is an almost unorthographic life One day
-                      however a small line of blind text by the name of Lorem
-                      Ipsum decided to leave for the far World of Grammar.
-                    </p>
-                  </div>
-                </div>
-              </article>
-
-              <article
-                className="timeline-entry animate-box"
-                data-animate-effect="fadeInTop"
-              >
-                <div className="timeline-entry-inner">
-                  <div className="timeline-icon color-4">
-                    <i className="icon-pen2"></i>
-                  </div>
-                  <div className="timeline-label">
-                    <h2>
-                      <a href="#">Creative Designer</a> <span>2017-2018</span>
-                    </h2>
-                    <p>
-                      Even the all-powerful Pointing has no control about the
-                      blind texts it is an almost unorthographic life One day
-                      however a small line of blind text by the name of Lorem
-                      Ipsum decided to leave for the far World of Grammar.
-                    </p>
-                  </div>
-                </div>
-              </article>
-
-              <article
-                className="timeline-entry animate-box"
-                data-animate-effect="fadeInLeft"
-              >
-                <div className="timeline-entry-inner">
-                  <div className="timeline-icon color-5">
-                    <i className="icon-pen2"></i>
-                  </div>
-                  <div className="timeline-label">
-                    <h2>
-                      <a href="#">UI/UX Designer at Envato</a>{" "}
-                      <span>2017-2018</span>
-                    </h2>
-                    <p>
-                      Even the all-powerful Pointing has no control about the
-                      blind texts it is an almost unorthographic life One day
-                      however a small line of blind text by the name of Lorem
-                      Ipsum decided to leave for the far World of Grammar.
-                    </p>
-                  </div>
-                </div>
-              </article>
-
-              <article
-                className="timeline-entry begin animate-box"
-                data-animate-effect="fadeInBottom"
-              >
-                <div className="timeline-entry-inner">
-                  <div className="timeline-icon color-none"></div>
-                </div>
-              </article>
+                </article>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </Element>
   );
 };
